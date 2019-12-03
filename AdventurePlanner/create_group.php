741422edcccc4d7ce70592a2f54481c7aaf21e08
Yukesh10 <yukesh_mynepal@yahoo.com>
2019-12-03 17:24:36 -0500

    added gear-branch stuff

--- AdventurePlanner/create_group.php
+++ AdventurePlanner/create_group.php
@@ -1,46 +1,35 @@
 <?php
     // Initialize the session
     session_start();
-
     // Uses basic user access level for viewing adventure details
     require_once "session_config.php";
-
     // Define variables and initialize with empty values
     $groupname = $description = $private = "";
     $groupname_err = $loggedin_err = $description_err = "";
     $group_id = 0;
-
     // Check to see if a user is logged in. If they are not, display an error and
     // do not let the user create a group
     if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
         $loggedin_err = "You must be logged in to create a group.";
     }
-
     // Process the form when the "Submit" button is pressed and a POST call is made
     if($_SERVER["REQUEST_METHOD"] == "POST"){
-
         // Make sure the group name is not empty
         if(empty(trim($_POST["groupname"]))){
             $groupname_err = "Please enter a name for your group.";
-        } 
-        else{
-
+        } else{
             // Prepare a SELECT statement to check that the group name
             // entered does not already exist in the database table "Groups"
-            $sql = "SELECT group_id FROM Groups WHERE group_name = ?";
-
+            $sql = "SELECT group_id FROM Groups WHERE group_name=?";
             if($stmt = mysqli_prepare($link, $sql)){
                 // Bind variables to the prepared statement as parameters
                 mysqli_stmt_bind_param($stmt, "s", $param_groupname);
-
                 // Set groupname parameter in the "mysqli_stmt_bind_param" function
                 $param_groupname = trim($_POST["groupname"]);
-
                 // Attempt to execute the prepared statement
                 if(mysqli_stmt_execute($stmt)){
                     // store result
                     mysqli_stmt_store_result($stmt);
-
                     // If the SELECT statement returns a result, the name is taken
                     // set the group name error variable
                     if(mysqli_stmt_num_rows($stmt) == 1){
@@ -49,21 +38,18 @@
                         $groupname = trim($_POST["groupname"]);
                     }
                 } else{
-                    echo "Oops! Something went wrong. Please try again later";
+                    echo "Oops! Something went wrong. Please try again later.";
                 }
             }
-
             // Close the statement made above in mysqli_prepare
             mysqli_stmt_close($stmt);
         }
-
         // Validate the description of the group and make sure it is not empty
         if(empty(trim($_POST["description"]))){
             $description_err = "Please provide a description for your group.";
         } else{
             $description = trim($_POST["description"]);
-        }
-
+        } 
         // Check the result of the checkbox in the form to decide whether to make the group private or not
         if(isset($_POST['privateCheckbox']) && $_POST['privateCheckbox'] == 'YES'){
         	$private = "Closed";
@@ -71,74 +57,80 @@
         else{
         	$private = "Open";
         }
-
         // If any of the errors checked have been set (i.e. the user is either not logged in, the group name is empty, or the description is empty) then don't let the user create a group.
         if(empty($groupname_err) && empty($description_err) && empty($loggedin_err)){
-
             // Prepare the SQL statement to INSERT the new group into the table
             $sql0 = "INSERT INTO Groups (group_name, description, status) VALUES (?, ?, ?)";
-
             if($stmt0 = mysqli_prepare($link, $sql0)){
-
                 // Bind variables to the prepared statement as parameters
                 mysqli_stmt_bind_param($stmt0, "sss", $param_groupname, $param_description, $param_status);
-
                 // Set parameters in the "mysqli_stmt_bind_param" function
                 $param_groupname = $groupname;
                 $param_description = $description;
                 $param_status = $private;
-
                 // Execute the statement anc insert the new group in the table
                 mysqli_stmt_execute($stmt0);
-
                 // If the insert didn't affect any rows, the group was not successfully created.
                 if(mysqli_stmt_affected_rows($stmt0) < 1){
                     echo "Could not create group";
                 } else{
                     $group_id = mysqli_insert_id($link);
+                    $_SESSION["group_id"] = $group_id;
+                    printf($stmt0->error);
                 }
-
+                // Close the statement made above in mysqli_prepare
+                mysqli_stmt_close($stmt0);
             }
-
-            // Close the statement made above in mysqli_prepare
-            mysqli_stmt_close($stmt0);
-
-
             // Prepare an insert statement for the belongs_to table
             // This will set the group owner to the user who created the group
-            $sql2 = "INSERT INTO belongs_to (user_id, group_id, membership_level) VALUES (?, ?, ?)";
-
+            $sql2 = "INSERT INTO belongs_to VALUES (?, ?, ?)";
             $access_level = "Owner";
-
             if($stmt2 = mysqli_prepare($link, $sql2)){
                 // Bind variables to the prepared statement as parameters
                 mysqli_stmt_bind_param($stmt2, "iis", $param_user_id, $param_group_id, $param_access_level);
-
                 // Set parameters in the "mysqli_stmt_bind_param" function
                 $param_user_id = $_SESSION["id"];
                 $param_group_id = $group_id;
                 $param_access_level = $access_level;
-
                 // Attempt to execute the prepared statement
                 if(mysqli_stmt_execute($stmt2)){
                     // Redirect to groups page upon success
-                    header("location: groups.php");
+                   //header("location: groups.php");
+                   echo("success");
                 } else{
                     echo "Something went wrong. Please try again later.";
                 }
+                // Close statement
+                mysqli_stmt_close($stmt2);
             }
-
-            // Close statement
-            mysqli_stmt_close($stmt2);
-
+            if(isset($_SESSION["array_name"])){
+                $array_name = $_SESSION["array_name"];
+            }
+            if (!empty($array_name)){
+                $hasQuery = '';
+                for ($count = 0; $count < count($array_name); $count++){
+                    $hasQuery .= '
+                        INSERT INTO Has VALUES("'.$group_id.'", "'.$array_name[$count].'");  
+                    ';
+                }
+                if ($hasQuery != ''){
+                    if (mysqli_multi_query($link, $hasQuery)){
+                        //unset($_SESSION['array_name']);
+                        header("location: groups.php");
+                    }
+                    else{
+                        echo 'Error';
+                    }
+                }
+                else{
+                    echo 'All Fields are Required';
+                }
+            }
+            
         }
-
         // Close connection
         mysqli_close($link);
-
     }
-
-
 ?>
 
 <!DOCTYPE html>
@@ -148,7 +140,7 @@
 
 		<!-- BootStrap CSS -->
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js">
 		<!-- Bootstrap JS -->
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
@@ -181,30 +173,12 @@
 		      <li class="nav-item">
 		        <a class="nav-link" href="adventures.php">Adventures </a>
 		      </li>
-					<li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="groups.php" id="navbardrop" data-toggle="dropdown">
-                        Trips
-                    </a>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="trips.php">All Trips</a>
-                        <a class="dropdown-item" href="create_trip.php">Create a Trip</a>
-                    </div>
-                </li>
-
-			<li class="nav-item dropdown">
-      				<a class="nav-link dropdown-toggle" href="groups.php" id="navbardrop" data-toggle="dropdown">
-        				Groups
-      				</a>
-      			 	<div class="dropdown-menu">
-                        <?php
-                          if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-                            echo "<a class='dropdown-item' href='groups.php?user_id=" . $_SESSION["id"] . "'> My Groups </a>";
-                          }
-                        ?>
-                        <a class="dropdown-item" href="groups.php">All Groups</a>
-        				<a class="dropdown-item" href="create_group.php">Create a Group</a>
-      				</div>
-   			</li>
+          <li class="nav-item">
+		        <a class="nav-link" href="trips.php"> Trips </a>
+		      </li>
+		      <li class="nav-item active">
+		        <a class="nav-link" href="groups.php"> Groups <span class="sr-only">(current)</span> </a>
+		      </li>
 		    </ul>
     		<?php
                 if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
@@ -220,27 +194,51 @@
 
 		  </div>
 		</nav>
+
 		<h1> Create Group </h1>
 
         <!-- This will display an error if the user is not logged in -->
         <span class="help-block"><?php echo $loggedin_err; ?></span>
 
         <!-- Form that will run the above PHP upon "submit" (POST) -->
-		<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+		<form id="done" name="done" method="POST">
                 <!-- Labels and input for "Group Name" insert -->
-	            <div class="form-group <?php echo (!empty($groupname_err)) ? 'has-error' : ''; ?>">
+	            <div class="form-group">
 	                <label>Group Name</label>
-	                <input type="text" name="groupname" class="form-control" value="<?php echo $groupname; ?>">
-	                <span class="help-block" style="color:red;text-align:center;"><?php echo $groupname_err; ?></span>
+	                <input type="text" name="groupname" class="form-control" required="true">
 	            </div>
 
                 <!-- Labels and input for "Description" insert -->
-	            <div class="form-group <?php echo (!empty($description_err)) ? 'has-error' : ''; ?>">
+	            <div class="form-group">
 	                <label>Description</label>
-	                <input type="text" name="description" class="form-control" value="<?php echo $description; ?>">
-	                <span class="help-block"><?php echo $description_err; ?></span>
+	                <input type="text" name="description" class="form-control" required="true">
 	            </div>
 
+                <div id="table_ready" class="form-group table-responsive">
+                    <label>Gear</label>
+                    <table class="table table-bordered" id="crud_table">
+                        <tr>
+                            <th width="10%">Name</th>
+                            <th width="10%">Type</th>
+                            <th width="10%">Status</th>
+                            <th width="10%">Brand</th>
+                            <th width="10%">Condition</th>
+                            <th width="5%"></th>
+                        </tr>
+                        <tr>
+                            <td contenteditable="true" class="name"></td>
+                            <td contenteditable="true" class="type"></td>
+                            <td contenteditable="true" class="status"></td>
+                            <td contenteditable="true" class="brand"></td>
+                            <td contenteditable="true" class="condition"></td>
+                            <td></td>
+                        </tr>
+                    </table>
+                    <div align="right">
+                        <button type="button" name="add" id="add" class="btn btn-success btn-xs">+</button> 
+                    </div> 
+                </div>
+ 
                 <!-- Checkbox for deciding if a group is public or private -->
 	            <div class="form-group form-check">
     				<input type="checkbox" class="form-check-input" name="privateCheckbox" value="YES" id="check">
@@ -249,11 +247,68 @@
 
                 <!-- Group that controls the submission of the form, triggers a POST call -->
 	            <div class="form-group">
-	                <input type="submit" class="btn btn-primary" value="Submit">
-	                <input type="reset" class="btn btn-default" value="Reset">
+	                <button type="submit" id="save" name="save" class="btn btn-primary">Submit</button>
+                    <input type="reset" class="btn btn-default" value="Reset">
 	            </div>
 	        </form>
-
 	</body>
-
+    <script>
+        $('#done').ready(function(){
+            var count = 1;
+            $('#add').click(function(){
+                count = count + 1;
+                var html_code = "<tr id='row"+count+"'>";
+                html_code += "<td contenteditable='true' class='name'></td>";
+                html_code += "<td contenteditable='true' class='type'></td>";
+                html_code += "<td contenteditable='true' class='status'></td>";
+                html_code += "<td contenteditable='true' class='brand'></td>";            
+                html_code += "<td contenteditable='true' class='condition'></td>";
+                html_code += "<td><button type='button' name='remove' data-row='row"+count+"' class='btn btn-danger btn-xs remove'>-</button></td>";
+                html_code += "</tr>";
+                $('#crud_table').append(html_code);  
+            });
+            $(document).on('click', '.remove', function(){
+                var delete_row = $(this).data("row");
+                $('#'+delete_row).remove();
+            });
+                $('#save').on("click", function(){  
+                    var name = [];
+                    var type = [];
+                    var status = [];
+                    var brand = [];
+                    var condition = [];
+                    $('.name').each(function(){
+                        name.push($(this).text());
+                    });
+                    $('.type').each(function(){
+                        type.push($(this).text());
+                    });
+                    $('.status').each(function(){
+                        status.push($(this).text());
+                    });
+                    $('.brand').each(function(){
+                        brand.push($(this).text());
+                    });
+                    $('.condition').each(function(){
+                        condition.push($(this).text());
+                    });
+                    jQuery.ajax({
+                        url:"insert.php",
+                        method: "POST",
+                        data:{
+                            name:name, 
+                            type:type, 
+                            status:status, 
+                            brand:brand, 
+                            condition:condition},
+                        success:function(result){
+                            //window.location.href = "groups.php";
+                        },
+                        error:function(result){
+                            alert("error");
+                        }
+                    });
+                });
+        });
+    </script>
 </html>
--- AdventurePlanner/group_details.php
+++ AdventurePlanner/group_details.php
@@ -8,7 +8,23 @@
 	// Uses basic user access level for viewing adventure details
 	require_once "session_config.php";
 
+	$profile_error = $profile_btn = "";
+	$param_id = $_SESSION["id"];
+    $query = "SELECT * FROM User_Profile WHERE user_id=$param_id LIMIT 1";
+    $result = mysqli_query($link, $query);
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+    $rows = mysqli_num_rows($result);
+    if ($rows > 0){
+       $profile_btn = "<input type='submit' id ='button' name='submit-button' class='btn btn-primary' value='Join this group'>";
+    }
+    else{
+       $profile_error = "<p> Some features will not be available until you create your user profile. To create your profile, please click <a href='profile.php'>here</a>. </p>";
+       $profile_btn = "<input type='submit' class='btn btn-primary' value='Join this group' disabled>";
+    }
+	mysqli_free_result($result);
+	
 	$group_id = $_GET['id'];
+
 
 	// Retrieves the group ID from the GET information in groups.php page
 	// Uses result of query to show information about the group in question
@@ -49,13 +65,73 @@
 
    	mysqli_free_result($result1);
 
+	$sql2 = "SELECT `gear_id`, `name`, `type`, `status`, `brand`, `condition` FROM Gear NATURAL JOIN Has WHERE group_id = ".$_GET['id'];
+   	$gear_result = mysqli_query($link, $sql2);
 	// Query to select all of the members of a group
-   	$sql2 = "SELECT user_id, name, membership_level FROM User_Profile NATURAL JOIN belongs_to NATURAL JOIN Groups WHERE group_id=".$_GET['id'];
+   	$sql3 = "SELECT user_id, name, membership_level FROM User_Profile NATURAL JOIN belongs_to NATURAL JOIN Groups WHERE group_id=".$_GET['id'];
 
    	// Store the query in the "result" variable to iteratively list the results
 	// in the HTML below
-   	$result2 = mysqli_query($link, $sql2);
-
+   	$result3 = mysqli_query($link, $sql3);
+	   if($_SERVER["REQUEST_METHOD"] == "POST"){
+		$sqlx = "INSERT INTO belongs_to VALUES (?, ?, ?)";
+        if($stmtx = mysqli_prepare($link, $sqlx)){
+            mysqli_stmt_bind_param($stmtx, "iis", $param_user_id, $param_group_id, $param_membership);
+			$param_user_id = $_SESSION["id"];
+			$param_group_id = $_GET['id'];
+			$param_membership = "Member";
+            if(mysqli_stmt_execute($stmtx)){
+                echo "success";
+            } else{
+                printf($stmtx->error);
+            }
+                // If the insert didn't affect any rows, the group was not successfully created.
+            mysqli_stmt_close($stmtx);
+		}
+		
+		$checkQuery = '';
+		$date = date('Y-m-d H:i:s');
+		//print($date);
+		$name = array();
+		$user_id = $_SESSION['id'];
+		$group_id = $_GET['id'];
+		$sql4 = "SELECT `gear_id`, `name`, `type`, `status`, `brand`, `condition` FROM Gear NATURAL JOIN Has WHERE group_id = ".$_GET['id'];
+   		$gear_r = mysqli_query($link, $sql4);
+		while($r = mysqli_fetch_array($gear_r)) {
+			array_push($name, $r['gear_id']);
+		}
+		if (isset($_POST['submit-btn'])){
+			if (!empty($name)){
+				for($count = 0; $count < count($name); $count++){
+					//$isDefined = false;
+					if(isset($_POST["privateCheckbox$count"])){
+						//$isDefined = true;
+						$checkQuery .= '
+                        	INSERT INTO checks_out VALUES("'.$user_id.'", "'.$name[$count].'", "'.$group_id.'", "'.$date.'", NULL);  
+                    	';
+					};
+				}
+				//echo "checkquery: " . $checkQuery . " .";
+				if ($checkQuery != ''){
+                    if (mysqli_multi_query($link, $checkQuery)){
+						//unset($_SESSION['array_name']);
+						//print("check: " . $checkQuery);
+						
+                    }
+                    else{
+						//echo "reporting";
+						echo "error";
+                    }
+                }
+                else{
+                    echo 'All Fields are Required';
+                }	
+			}
+		}
+		else{
+			echo "not pressed yet!";
+		}
+	}
 ?>
 
 <!DOCTYPE html>
@@ -65,7 +141,7 @@
 
 		<!-- BootStrap CSS -->
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
+		<link rel="stylesheet" href="css/style.css">
 		<!-- Bootstrap JS -->
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
@@ -152,36 +228,109 @@
 		<p> Status: <?php echo $group_status; ?>  </p>
 		<p> Description: <?php echo $group_description; ?> </p>
 
+		<p> Gear : The available gears are: </p>
+		<div class="table-responsive">
+		<table class="table table-bordered" id="crud_table">
+			<thead>
+				<th scope="col">Name</th>
+				<th scope="col">Type</th>
+				<th scope="col">Status</th>
+				<th scope="col">Brand</th>
+				<th scope="col">Condition</th>
+			</thead>
+
+			<tbody>
+				<?php
+				$name = array();
+				while($row = mysqli_fetch_array($gear_result)) {
+					array_push($name, $row['name']);
+					echo "<tr>";
+					echo "<td>" . $row['name'] . "</td>";
+					echo "<td>" . $row['type'] . "</td>";
+					echo "<td>" . $row['status'] . "</td>";
+					echo "<td>" . $row['brand'] . "</td>";
+					echo "<td>" . $row['condition'] . "</td>";
+					echo "</tr>";
+				}
+				?>
+			</tbody>
+		</table>
+		</div>
+			<!-- <form method="post"> -->
+			<?php echo $profile_btn; ?>
+			<?php echo $profile_error; ?>
+			<!-- </form> -->
+		</div>
+
+		<div class="bg-modal">
+			<div class="modal-content">
+				<div class="modal-close">+</div>	
+				<h1>Gear List</h1>
+				<form method="post">
+				<ul id="list" class="list-group overflow-auto">
+					<?php
+						for($i = 0; $i < count($name); $i++){
+							echo "<li>
+								<div class=\"form-check\">
+								<div class=\"form-group form-check\">
+									<input type=\"checkbox\" class=\"form-check-input\" name=\"privateCheckbox$i\" value=\"YES\" id=\"gear$i\">
+									<label class=\"form-check-label\" for=\"gear$i\">$name[$i]</label>
+								</div>
+								</li>";
+							}
+						?>
+				</ul>	
+					<div class="submit-btn">
+						<input type="submit" id="submit-btn" name="submit-btn" class="btn btn-primary" value="submit">
+					</div>
+				</form>
+			</div>
+		</div>	
 		<!-- Shows the members of a group -->
 		<h2>Members:</h2>
 
 		<!-- Table for showing the results of the query on group users -->
-		<table class="table table-hover table-sm table-striped">
+		<table class="table table-hover table-sm">
 
 			<!-- Header of the table -->
 			<thead>
 				<th scope="col">Name</th>
 				<th scope="col">Role</th>
+				<th scope="col">Gear</th>
 			</thead>
 
 			<!-- Body of the table, uses PHP to show the results of the query
 			 by using the $result variable defined in the PHP section of the file above -->
 			<tbody>
 				<?php
-
-					while($row = mysqli_fetch_array($result2)) {
+					while($row = mysqli_fetch_array($result3)) {
 					echo "<tr>";
 					echo "<td> <a class='btn btn-link' role='button' href='user_details.php?id=" . $row['user_id'] . "'>" . $row['name'] .  "</td>";
 					echo "<td>" . $row['membership_level'] . "</td>";
+					$sql4 = "SELECT `name`, `user_id` FROM checks_out NATURAL JOIN Gear WHERE user_id=".$row['user_id'] ." AND group_id=".$_GET['id'];
+					$result4 = mysqli_query($link, $sql4);
+					$value = '';
+					while ($row4 = mysqli_fetch_array($result4)){
+						$value .= $row4['name'] . " , ";
+					}
+					echo "<td>" . $value . "</td>";	
+					echo "<td>" . "</td>"; 
 					echo "</tr>";
 					}
-					mysqli_close($con);
-
+					//mysqli_close($con);
 			 	?>
 		 	</tbody>
 
 		</table>
 
+	<script>
+		document.getElementById('button').addEventListener('click', function(){
+			document.querySelector('.bg-modal').style.display = 'flex';
+		});
+		document.querySelector('.modal-close').addEventListener('click', function(){
+			document.querySelector('.bg-modal').style.display = 'none';
+		});
+	</script>
 	</body>
 
 </html>
